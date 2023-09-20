45cf9ae61f85837c841f456d58e1d6d6e9537f4b
njw4803 <njw4803@naver.com>
2023-09-20 23:58:25 +0900

    프로젝트 기본 세팅

---
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -0,0 +1,6 @@
+package com.cos.jwt.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
